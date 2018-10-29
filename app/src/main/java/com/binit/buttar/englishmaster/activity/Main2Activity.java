0f9a37f47b0f2abc658532e3517b0ba0a3a81040
SumanpreetLubana <preetlubana5397@gmail.com>
2018-10-29 16:23:12 +0530

    Second

---
+++ app/src/main/java/com/binit/buttar/englishmaster/activity/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.binit.buttar.englishmaster.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.binit.buttar.englishmaster.R;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
--- app/src/main/java/com/binit/buttar/englishmaster/activity/Test.java
+++
@@ -1,4 +0,0 @@
-package com.binit.buttar.englishmaster.activity;
-
-public class Test {
-}
